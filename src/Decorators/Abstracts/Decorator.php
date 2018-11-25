0df2f9489aeee925373c9ab6c7f11bf3d0670ee0
Gerald Buttinger <41453547+codeflorist@users.noreply.github.com>
2018-11-25 21:48:18 +0100

    Update Decorator.php

--- src/Decorators/Abstracts/Decorator.php
+++ src/Decorators/Abstracts/Decorator.php
@@ -13,6 +13,18 @@ abstract class Decorator
      * @var Element
      */
     protected $element;
+    
+    /**
+     * Applies this decorator immediately
+     * before it's parent (instead of after).
+     * Only relevant, if $this->getParentDecorator()
+     * actually returns a parent.
+     * 
+     * TODO: Make this work.
+     *
+     * @var bool
+     */
+    protected $applyBeforeParent = false;    
 
     /**
      * Decorator constructor.
@@ -44,5 +56,19 @@ abstract class Decorator
      * Perform decorations on $this->element.
      */
     public abstract function decorate();
+    
+    /**
+     * Overwrite to return Full Qualified Class Name
+     * of Parent Decorator (must extend Decorator class).
+     * $this decorator is applied immediately after it's parent,
+     * (or before, if $applyBeforeParent is set to true).
+     *
+     * TODO: Make this work.
+     *
+     */
+    public function getParentDecorator()
+    {
+        return null;
+    }
 
 }
